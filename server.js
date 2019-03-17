a3f8931fc99cd5565e2d872af1591931cecd5b1a
dreamlabo <toddlabo@gmail.com>
2019-03-17 00:36:16 -0600

    Second commit

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ router.post('/signup', function(req, res) {
         });
     }
 });
-
+///////
 router.post('/signin', function(req, res) {
     var userNew = new User();
     userNew.name = req.body.name;
